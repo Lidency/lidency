e40241cbea42e7f3d370e089f80d54404caa68f6
snooker4real <cindanojonathan@icloud.com>
2023-04-22 02:19:40 +0200

    adding scroll animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,3 +56,20 @@ window.addEventListener("scroll", function () {
     backTopBtn.classList.remove("active");
   }
 });
+
+/**
+ *  hidden section entities
+ */
+const observer = new IntersectionObserver(function (entries) {
+    entries.forEach(entry => {
+      console.log(entry);
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
